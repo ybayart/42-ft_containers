f5f50f9da6f3ab7c06a26f3d1857d5e6b2fd1c78
ybayart <ybayart@student.42.fr>
2020-05-14 21:47:08 +0200

    patch osx

--- vector.hpp
+++ vector.hpp
@@ -130,10 +130,10 @@ namespace	ft
 				typedef typename allocator_type::const_reference	const_reference;
 				typedef typename allocator_type::pointer			pointer;
 				typedef typename allocator_type::const_pointer		const_pointer;
-				typedef normal_iterator<pointer, vector>			iterator;
-				typedef normal_iterator<const_pointer, vector>		const_iterator;
-				typedef std::reverse_iterator<iterator>				reverse_iterator;
-				typedef std::reverse_iterator<const_iterator>		const_reverse_iterator;
+				typedef ft::normal_iterator<pointer, vector>		iterator;
+				typedef ft::normal_iterator<const_pointer, vector>	const_iterator;
+				typedef ft::reverse_iterator<iterator>				reverse_iterator;
+				typedef ft::reverse_iterator<const_iterator>		const_reverse_iterator;
 				typedef ptrdiff_t									difference_type;
 				typedef size_t										size_type;
 
